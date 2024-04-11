8ee81531c234c1baa58ed25507201fa747e68e7c
supriya <supriya@ideaelan.com>
2024-04-11 17:15:38 +0530

    Modifications

--- src/test/java/Selenium/LaunchBrowser.java
+++ src/test/java/Selenium/LaunchBrowser.java
@@ -1,5 +1,6 @@
 package Selenium;
 
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,9 @@ public class LaunchBrowser {
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
 		Thread.sleep(3000);
 		driver.close();
