4c8c48704d8dda4aeb0bb424d744b7b7603377ef
supriya <supriya@ideaelan.com>
2024-04-11 16:46:59 +0530

    First commit

---
+++ src/test/java/Selenium/LaunchBrowser.java
@@ -0,0 +1,17 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+		Thread.sleep(3000);
+		driver.close();
+
+	}
+
+}
